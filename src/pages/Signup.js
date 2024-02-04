034fff6139479046ac7fb98b979be814f836a512
Ram S Mane <100112083+Ram-mane@users.noreply.github.com>
2024-02-04 14:01:33 +0530

    signUp error fixed

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -32,7 +32,7 @@ const Signup=()=>{
 
 
   // submit form
-  const submitForm=(event)=>{
+  const submitForm= (event)=>{
     event.preventDefault()
     console.log(data);
 
@@ -40,27 +40,20 @@ const Signup=()=>{
 
     //call server api for sending data
     signUp(data).then((resp)=>{
-      console.log(data)
       console.log("Success log");
     }).catch((error)=>{
       console.log(error)
       console.log("Error log");
-
-      
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.error("Response data:", error.response.data);
-        console.error("Response status:", error.response.status);
-        console.error("Response headers:", error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.error("Request data:", error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error("Error message:", error.message);
-      }
     })
+
+    // event.preventDefault()
+    // try {
+    //   const response = await signUp(data);
+    //   console.log(response?.data);
+
+    // } catch (error) {
+    //   console.error(error);
+    // }
 
 
   };
